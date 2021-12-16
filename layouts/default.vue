30f30b78e43e65ffe54458d5b7a43d9cb984e942
WachirananNot <wachiranan.pu@gmail.com>
2021-12-16 21:15:23 +0700

    add-spacer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
  <v-toolbar dark>
    <v-toolbar-title>ระบบจองห้อง</v-toolbar-title>
       <v-toolbar-item><v-btn>จองห้อง</v-btn></v-toolbar-item>
-      
+      <v-spacer></v-spacer>
       <v-icon>mdi-magnify</v-icon>
       
   </v-toolbar>
