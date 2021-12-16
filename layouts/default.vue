30a2efa8b6d11461f8114515def6ae8ba846d20b
sutthiphanKeang <kang.kang11909@gmail.com>
2021-12-16 20:57:37 +0700

    test

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+ <v-toolbar dark>
+   <v-toolbar-title>ระบบจองห้อง</v-toolbar-title>
+      <v-toolbar-item><v-btn>จองห้อง</v-btn></v-toolbar-item>
+      
+      <v-icon>mdi-magnify</v-icon>
+      
+  </v-toolbar>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
