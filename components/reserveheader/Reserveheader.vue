e0be9346df8925e31832dc79b5b5575630a3fa89
Pichai (Phumin) <pn@manaosoftware.com>
2021-12-17 12:51:29 +0700

    create reserve page

---
+++ components/reserveheader/Reserveheader.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>หน้าจองห้องจย้า</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
---
+++ pages/reserve/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container><Reserveheader /></v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
