36e26ba3fd79e1b7ec352bbcd69a0bc870d62690
Pichai (Phumin) <pn@manaosoftware.com>
2021-12-17 12:52:04 +0700

    Create reserve list page

---
+++ components/reservelistheader/Reservelistheader.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>หน้ารายการจย้า</h1>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/reserved-list/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <v-container><Reservelistheader /></v-container>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
