c705aaab84be4a6a9d9ac49579747b5d907fbbcd
Pichai (Phumin) <pn@manaosoftware.com>
2021-12-17 15:44:46 +0700

    resort property   level

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="hidden-xs-only">
-        <v-btn flat v-for="item in menuItems" :key="item.title" :to="item.path">
+        <v-btn v-for="item in menuItems" :key="item.title" :to="item.path" flat>
           <v-icon left dark>{{ item.icon }}</v-icon>
           {{ item.title }}
         </v-btn>
