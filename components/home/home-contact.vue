b0c4e6b2e4ea4fd2366167e43e63ba182b36e0f1
sutthiphanKeang <kang.kang11909@gmail.com>
2022-01-28 00:13:25 +0700

    add kinesis part welcome and contact

---
+++ components/home/home-contact.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-card dark>
+      <v-card-title class="d-flex justify-center"> Contact </v-card-title>
+      <v-card-text class="text-center">
+        <div>Pichai Namwan</div>
+        <div>0932895079</div>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,66 +2,22 @@
   <v-container>
     <v-row class="block1">
       <v-col cols="3">
-        <div class="box">
-          <div class="LCD d-flex justify-center">
-            <div class="hours">{{ hours }}</div>
-            <div class="divider">:</div>
-            <div class="minutes">{{ minutes }}</div>
-            <div class="divider">:</div>
-            <div class="seconds">{{ seconds }}</div>
-          </div>
-        </div>
-        <v-date-picker v-model="picker" dark full-width></v-date-picker>
+        <home-calendar />
+      </v-col>
+      <v-col align-self = 'center'>
+        <home-kinesis />
+      </v-col>
+    </v-row>
+    <v-row>
+      <v-col>
+        <home-contact />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      picker: new Date(Date.now() - new Date().getTimezoneOffset() * 60000)
-        .toISOString()
-        .substr(0, 10),
-      hours: 0,
-      minutes: 0,
-      seconds: 0,
-    }
-  },
-  mounted() {
-    setInterval(() => this.setTime(), 1000)
-  },
-  methods: {
-    setTime() {
-      const date = new Date()
-      let hours = date.getHours()
-      let minutes = date.getMinutes()
-      let seconds = date.getSeconds()
-      hours = hours <= 9 ? `${hours}`.padStart(2, 0) : hours
-      minutes = minutes <= 9 ? `${minutes}`.padStart(2, 0) : minutes
-      seconds = seconds <= 9 ? `${seconds}`.padStart(2, 0) : seconds
-      this.hours = hours
-      this.minutes = minutes
-      this.seconds = seconds
-    },
-  },
-}
+export default {}
 </script>
 
-<style scoped>
-.LCD > div {
-  font-family: 'alarm clock';
-  font-size: 50px;
-  color: aliceblue;
-  text-shadow: 0 0 0.2em rgb(88, 88, 88), 0 0 0.2em rgb(88, 88, 88),
-    0 0 0.2em rgb(88, 88, 88);
-}
-.box {
-  background-color: rgba(0, 0, 0, 0.75);
-  border: 1px solid darkgrey;
-  border-radius: 7px;
-  margin-bottom: 20px;
-  margin-top: 10px;
-}
-</style>
+<style scoped></style>
